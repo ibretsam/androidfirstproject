98f1488c368844d905884d3d8dc7eddfdabfaf30
Khanh Le <91043526+ibretsam@users.noreply.github.com>
2022-11-04 15:52:49 +0700

    Added logout button

--- app/src/main/java/com/example/androidfirstproject/Views/HomeActivity.java
+++ app/src/main/java/com/example/androidfirstproject/Views/HomeActivity.java
@@ -3,22 +3,30 @@ package com.example.androidfirstproject.Views;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.androidfirstproject.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
     private static int SPLASH_SCREEN = 5000;
 
+    FirebaseAuth mAuth;
+
     Animation topAnim, bottomAnim;
     ImageView image;
     TextView logo, slogan;
+    Button btnLogout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +41,18 @@ public class HomeActivity extends AppCompatActivity {
         image = (ImageView) findViewById(R.id.imageView);
         logo = (TextView) findViewById(R.id.textView);
         slogan = (TextView) findViewById(R.id.textView2);
+        btnLogout = (Button) findViewById(R.id.btnLogout);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                startActivity(new Intent(HomeActivity.this, RegisterActivity.class));
+                finish();
+            }
+        });
 
         // Animat
         image.setAnimation(topAnim);
