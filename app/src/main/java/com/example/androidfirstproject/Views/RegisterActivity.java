28257b882c4a4c9738a890e817c36478d21321ea
Khanh Le <91043526+ibretsam@users.noreply.github.com>
2022-11-04 15:53:10 +0700

    Implement Phone Number Authentication

--- app/src/main/java/com/example/androidfirstproject/Views/RegisterActivity.java
+++ app/src/main/java/com/example/androidfirstproject/Views/RegisterActivity.java
@@ -1,16 +1,152 @@
 package com.example.androidfirstproject.Views;
 
-import android.os.Bundle;
+import static android.content.ContentValues.TAG;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.androidfirstproject.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class RegisterActivity extends AppCompatActivity {
+    TextInputEditText edtPhone, edtOTP;
+    Button btnRegister, btnVerify;
+    FirebaseAuth rAuth;
+    String verificationID;
+    LinearLayout lytOTP;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        edtPhone = findViewById(R.id.edtPhone);
+        edtOTP = findViewById(R.id.edtOTP);
+        btnVerify = findViewById(R.id.btnVerify);
+        btnRegister = findViewById(R.id.btnRegister);
+        lytOTP = findViewById(R.id.lytOTP);
+        progressBar = findViewById(R.id.progressBar);
+        rAuth = FirebaseAuth.getInstance();
+
+        btnVerify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phone = "+84" + edtPhone.getText().toString().trim();
+
+                if (TextUtils.isEmpty(phone)) {
+                    edtPhone.setError("Phone number is required.");
+                    return;
+                }
+
+                sendVerificationCode(phone);
+                progressBar.setVisibility(View.VISIBLE);
+                lytOTP.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String otp = edtOTP.getText().toString().trim();
+                if (TextUtils.isEmpty(otp)) {
+                    edtOTP.setError("Please enter your OTP.");
+                    return;
+                }
+
+                verifyCode(otp);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            startActivity(new Intent(RegisterActivity.this, HomeActivity.class));
+            finish();
+        }
+    }
+
+    private void sendVerificationCode(String phoneNumber) {
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(rAuth)
+                        .setPhoneNumber(phoneNumber)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onVerificationCompleted(@NonNull PhoneAuthCredential credential) {
+            final String code = credential.getSmsCode();
+            if (code != null) {
+                verifyCode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(@NonNull FirebaseException e) {
+            Toast.makeText(RegisterActivity.this, "Verification Failed.", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onCodeSent(@NonNull String s,
+                @NonNull PhoneAuthProvider.ForceResendingToken token) {
+            super.onCodeSent(s, token);
+            verificationID = s;
+            Toast.makeText(RegisterActivity.this, "OTP code has sent to your phone number", Toast.LENGTH_SHORT);
+            btnRegister.setEnabled(true);
+            progressBar.setVisibility(View.INVISIBLE);
+        }
+    };
+
+    private void verifyCode(String code) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationID, code);
+        signInByCredentials(credential);
+    }
+
+    private void signInByCredentials(PhoneAuthCredential credential) {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, "Login successfull", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegisterActivity.this, HomeActivity.class));
+                        }
+                    }
+                });
     }
 }
