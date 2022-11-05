ab2f7ed326e3cc050d89f9e3d4c25bc75261a4a2
Comchiennuocmam <117425994+Comchiennuocmam@users.noreply.github.com>
2022-11-05 22:53:50 +0700

    thích gì ghi nấy :))

---
+++ app/src/main/java/com/example/androidfirstproject/ChatApp/HomeMessage.java
@@ -0,0 +1,17 @@
+package com.example.androidfirstproject.ChatApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.androidfirstproject.R;
+
+public class HomeMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+    }
+}
