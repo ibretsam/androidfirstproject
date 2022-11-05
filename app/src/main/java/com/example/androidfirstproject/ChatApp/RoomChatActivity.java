242a7ccbcc92e3489304fb8f34ff250e858f8ab9
Comchiennuocmam <117425994+Comchiennuocmam@users.noreply.github.com>
2022-11-05 23:34:34 +0700

    giao diện đã đủ

---
+++ app/src/main/java/com/example/androidfirstproject/ChatApp/RoomChatActivity.java
@@ -0,0 +1,16 @@
+package com.example.androidfirstproject.ChatApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.androidfirstproject.R;
+
+public class RoomChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_chat);
+    }
+}
