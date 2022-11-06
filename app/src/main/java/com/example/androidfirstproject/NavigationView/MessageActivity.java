c170e67d49f06357b48dbee24f093c4605a170f4
Comchiennuocmam <117425994+Comchiennuocmam@users.noreply.github.com>
2022-11-06 23:28:39 +0700

    đã thêm xem lại coi có lỗi j ko báo cho em nhé mn

---
+++ app/src/main/java/com/example/androidfirstproject/NavigationView/MessageActivity.java
@@ -0,0 +1,44 @@
+package com.example.androidfirstproject.NavigationView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.androidfirstproject.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+        // Initialize And Assign Varible
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Set Home Selected
+        bottomNavigationView.setSelectedItemId(R.id.messege);
+        // Perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.messege:
+                        return true;
+                    case R.id.account:
+                        startActivity(new Intent(getApplicationContext(), PhoneBookActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.contact:
+                        startActivity(new Intent(getApplicationContext(), AccountActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/androidfirstproject/NavigationView/PhoneBookActivity.java
@@ -0,0 +1,44 @@
+package com.example.androidfirstproject.NavigationView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.androidfirstproject.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class PhoneBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recycle_phone);
+        // Initialize And Assign Varible
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Set Home Selected
+        bottomNavigationView.setSelectedItemId(R.id.account);
+        // Perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.messege:
+                        startActivity(new Intent(getApplicationContext(), MessageActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.account:
+                        return true;
+                    case R.id.contact:
+                        startActivity(new Intent(getApplicationContext(), AccountActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
