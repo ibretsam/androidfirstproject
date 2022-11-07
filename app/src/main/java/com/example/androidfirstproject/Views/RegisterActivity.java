31d25b5262fa63f37cf281da694a29fa38a4155e
Khanh Le <masterbruculous@gmail.com>
2022-11-07 09:14:52 +0700

    Edit switch activity

--- app/src/main/java/com/example/androidfirstproject/Views/RegisterActivity.java
+++ app/src/main/java/com/example/androidfirstproject/Views/RegisterActivity.java
@@ -93,7 +93,7 @@ public class RegisterActivity extends AppCompatActivity {
         // Kiểm tra xem đã có đăng nhập hay chưa, nếu có thì chuyển sang HomeActivity
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
-            startActivity(new Intent(RegisterActivity.this, HomeActivity.class));
+            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
             finish();
         }
     }
