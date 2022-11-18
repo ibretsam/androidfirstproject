a7057cd705f649451514ff4c2e7af6031166695f
Khanh Le <masterbruculous@gmail.com>
2022-11-18 09:08:18 +0700

    Added add contact to phonebook

---
+++ app/src/main/java/com/example/androidfirstproject/Views/NavigationViews/OnCompleteCallback.java
@@ -0,0 +1,7 @@
+package com.example.androidfirstproject.Views.NavigationViews;
+
+import com.example.androidfirstproject.Models.User;
+
+public interface OnCompleteCallback {
+    void onComplete(User phoneUser);
+}
