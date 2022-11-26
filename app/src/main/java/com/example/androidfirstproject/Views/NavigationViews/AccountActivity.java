4c4eb7624904c10666ae785aa0e7f292bb04ebf0
linmynil <117365289+linmynil@users.noreply.github.com>
2022-11-26 11:13:46 +0700

    update create account

--- app/src/main/java/com/example/androidfirstproject/Views/NavigationViews/AccountActivity.java
+++ app/src/main/java/com/example/androidfirstproject/Views/NavigationViews/AccountActivity.java
@@ -8,20 +8,30 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.androidfirstproject.Models.User;
 import com.example.androidfirstproject.R;
 import com.example.androidfirstproject.Views.Auth.PhoneInputActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AccountActivity extends AppCompatActivity {
     private Button logoutBtn;
-    private String currentUserID;
+    private String currentUserID,nameUser1,CurrentPhoneUser1;
+    private DatabaseReference mDatabase;
+    TextView myPhone, myName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +39,10 @@ public class AccountActivity extends AppCompatActivity {
         setContentView(R.layout.activity_account);
         // Initialize And Assign Varible
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-
+        myPhone = findViewById(R.id.myPhone);
+        myName = findViewById(R.id.myName);
         logoutBtn = findViewById(R.id.logoutBtn);
+
 
         if ( currentUserID == null) {
             FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -42,6 +54,7 @@ public class AccountActivity extends AppCompatActivity {
                 Log.d(TAG, "CurrentUserID Error: " + e.getMessage());
             }
         }
+account();
 
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,6 +84,26 @@ public class AccountActivity extends AppCompatActivity {
                         return true;
                 }
                 return false;
+            }
+        });
+
+
+    }
+    public void account(){
+        mDatabase = FirebaseDatabase.getInstance().getReference("user").child(currentUserID);
+        mDatabase.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                } else {
+                    User user = task.getResult().getValue(User.class);
+                    nameUser1 = user.getFullName();
+                    CurrentPhoneUser1 = user.getPhoneNumber();
+                    myName.setText(nameUser1);
+                    myPhone.setText(CurrentPhoneUser1);
+                    Log.d(">>>TAG",""+nameUser1+CurrentPhoneUser1);
+                }
             }
         });
     }
